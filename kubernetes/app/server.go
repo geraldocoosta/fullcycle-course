38f65125b4b1751a30c88440cb0550aec28e70bd
Geraldo Costa <geraldo.costa-ext@ab-inbev.com>
2022-07-03 16:35:41 -0300

    Utilizando váriaveis de ambiente

--- kubernetes/app/server.go
+++ kubernetes/app/server.go
@@ -1,9 +1,19 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+		name := os.Getenv("NAME")
+		age := os.Getenv("AGE")
+
+		fmt.Fprintf(w, "Hello I'm %s and I'm %s years old", name, age)
+
 		w.Write([]byte("Hello, Kubernetes!!"))
 	})
 	http.ListenAndServe(":8080", nil)
